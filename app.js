8427141e8d922c9b0beda061c65507a791e03deb
t0336000d <leo.qiyi.joel@dhs.sg>
2016-07-29 16:32:45 +0800

    third commit

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 $(function() {
-// code for clicking button .etc
-
     $("#getJoke").click(
         function() {
             $.get("/jokes",function(data){
-                // TODO:
-              
-                   $("#setup").html(data.setup);
-                   $("#punchline").html(data.punchline);
+                $("#setup").html(data.setup);
+                $("#punchline").html(data.punchline);
             },"json")
         }
     );
